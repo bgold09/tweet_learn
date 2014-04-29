725c35271a4f918d654159cc0c53bebef3a26628
d2b2 <d2b2@d2b2-Aspire-7745G.(none)>
2014-04-29 14:27:43 -0400

    the second training/testing set is stored and has been testeted. Next step is cross validation.

--- tweet_learn.py
+++ tweet_learn.py
@@ -16,15 +16,18 @@ import gensim as gs
 
 tweet_subset = "tweetSubset_danielle.csv"
 transformed_set = "features.csv"
+train_test_set = "dani_tweets.csv"
     
 def fit_plot(ml):
     """use a SVM to create a classifier and make a plot of the data points"""
-    clf = svm.SVC(kernel='linear')
-    clf.fit(ml[0], ml[1])
+    X = ml[0]
+    Y = ml[1]
+    clf = svm.SVC(kernel='linear', C=1).fit(ml[0], ml[1])
+    
     w = clf.coef_[0]
-    a = -w[0] / w[1]
     xx = np.linspace(-5, 5)
-    yy = a * xx - (clf.intercept_[0]) / w[1]
+    yy = np.dot(w, xx) - (clf.intercept_[0]) / np.dot(w, w)
+
     b = clf.support_vectors_[0]
     yy_down = a * xx + (b[1] - a * b[0])
     b = clf.support_vectors_[-1]
@@ -32,18 +35,20 @@ def fit_plot(ml):
     pl.plot(xx, yy, 'k-')
     pl.plot(xx, yy_down, 'k--')
     pl.plot(xx, yy_up, 'k--')
-    
-    pl.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
-               s=80, facecolors='none')
-    pl.scatter(ml[0][:, 0], ml[0][:, 1], c=ml[1], cmap=pl.cm.Paired)
-    pl.axis('tight')
-#    pl.axis([-2, 2, -50, 50])
+
+    pl.plot(xx, yy, 'k-')
+    pl.plot(xx, yy_down, 'k--')
+    pl.plot(xx, yy_up, 'k--')
+
+    pl.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80, facecolors='none')
+    pl.scatter(X[:, 0], X[:, 1], c=Y, cmap=pl.cm.Paired)
+
     pl.show()
 
     return clf
 #---------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------
-def extract_transform_data():
+def extract_transform_data(tbl_name, a, b):
     """PRE:  java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/ner-eng-ie.crf-3-all2008-distsim.ser.gz -port 8080 -outputFormat inlineXML  --------- MUST BE RUNNING to get the Named-Entity data. 
              The other data comes from the SQL table init_data     
       POST: populate the NER columns in our dataset"""
@@ -54,7 +59,7 @@ def extract_transform_data():
     labels = list()
     tagger = ner.SocketNER(host='localhost', port=8080)
 
-    cur.execute("select * from init_data")
+    cur.execute("select * from {0} limit %s, %s".format(tbl_name), (a, b))
     rows = cur.fetchall()
 
     for row in rows:
@@ -90,8 +95,9 @@ def extract_transform_data():
             continue
 
 #        li = [website, ret_cnt, rep, ret, num_people, num_orgs, num_locs, cent]
-        li = [ret_cnt, rep, num_people, num_locs, cent]
+        li = [website, ret_cnt, rep, ret, num_people, num_orgs, num_locs, cent]
         label = row["I_c"]
+
         labels.append(label)
         tweet_set.append(li)
     
@@ -118,32 +124,38 @@ def store_features(tweet_set):
     con.close()
 #---------------------------------------------------------------------
 #---------------------------------------------------------------------
-def store_initial_data():
+def store_initial_data(tbl_name):
+    """populate SQL table 'tbl_name' with CSV 'train_test_set'"""
     con = mdb.connect(host="localhost", user="root", passwd="", db="twitter") 
     cur = con.cursor(mdb.cursors.DictCursor)
-    cur.execute("drop table if exists init_data;")
-    cur.execute("create table init_data (tweet_id bigint, source_user_id bigint, rt_user_id bigint, tweet varchar(255), website varchar(140), tweet_time timestamp, raw_retweet_count bigint, I_c smallint);")
+    cur.execute("drop table if exists {0};".format(tbl_name))
+    cur.execute("create table {0} (tweet_id bigint, source_user_id bigint, rt_user_id bigint, tweet varchar(255), website varchar(140), tweet_time timestamp, raw_retweet_count bigint, I_c smallint);".format(tbl_name))
 
-    os.chdir("data")
-    with open(tweet_subset, 'r') as csvfile:
+    with open("data/"+train_test_set, 'r') as csvfile:
         tweet_reader = csv.DictReader(csvfile, delimiter = '\t')
         tweet_reader.next()
 
         for row in tweet_reader:
-            cur.execute("insert into init_data (tweet_id, source_user_id, rt_user_id, tweet, website, tweet_time, raw_retweet_count, I_c) values (%s, %s, %s, %s, %s, %s, %s, %s)", (row["tweet_id"], row["source_user_id"], row["rt_user_id"], row["tweet"], row["website"], row["tweet_time"], row["raw_retweet_count"], row["I_c"]))
+            ic = row["I_c"]
+            if ic == 'i':                  #informative is 1
+                ic = 1                   
+            elif ic == 'c':                          #conversational is -1
+                ic = -1
+            else:
+                continue
+            cur.execute("insert into {0} (tweet_id, source_user_id, rt_user_id, tweet, website, tweet_time, raw_retweet_count, I_c) values (%s, %s, %s, %s, %s, %s, %s, %s)".format(tbl_name), (row["tweet_id"], row["source_user_id"], row["rt_user_id"], row["tweet"], row["website"], row["tweet_time"], row["raw_retweet_count"], ic))
 
     con.commit()
     cur.close()
     con.close()
-    os.chdir("../")
 #---------------------------------------------------------------------
 #---------------------------------------------------------------------
-def add_centrality_feature():
+def add_centrality_feature(tbl_name):
     con = mdb.connect(host="localhost", user="root", passwd="", db="twitter") 
     cur = con.cursor(mdb.cursors.DictCursor)
     
-#    cur.execute("alter table init_data add column eig_centrality float")
-    cur.execute("update init_data id inner join users u on id.source_user_id = u.user_id set id.eig_centrality = u.eigenvector_centrality")
+    cur.execute("alter table {0} add column eig_centrality float".format(tbl_name))
+    cur.execute("update {0} id inner join users u on id.source_user_id = u.user_id set id.eig_centrality = u.eigenvector_centrality".format(tbl_name))
     con.commit()
     cur.close()
     con.close()
@@ -151,10 +163,10 @@ def add_centrality_feature():
 #---------------------------------------------------------------------            
 def get_dict_corpus():
     """return and serialize dictionary and corpus from tweets"""
-    con = mdb.connect(host="localhost", user="root", passwd="", db="twitter") 
+    con = mdb.connect(host="localhost", user="root", passwd="", db="twitter", charset='utf8') 
     cur = con.cursor(mdb.cursors.DictCursor)
 
-    cur.execute("select tweet from tweets order by RAND() limit 1000")
+    cur.execute("select tweet from init data")
     rows = cur.fetchall()
     tweets = list()
     for row in rows:
