08746ab4231681df6cbf6322bfab2a624ea80955
Brian Golden <brian.golden09@gmail.com>
2014-04-30 15:54:51 -0400

    minor formatting

--- score.py
+++ score.py
@@ -7,14 +7,14 @@ import random
 def cross_validation(n, features, labels, penalty):
     """return the scores for 'n' fold cross validation
     
-        Args:
-            n: number of folds
-            features: feature matrix
-            labels: labels for feature matrix
-            penalty: penalty parameter for classifier
+    Args:
+        n: number of folds
+        features: feature matrix
+        labels: labels for feature matrix
+        penalty: penalty parameter for classifier
 
-        Returns:
-            scores for this permutation test
+    Returns:
+        scores for this permutation test
     """
     clf = svm.SVC(kernel='linear', C=penalty).fit(features, labels)
     X_folds = np.array_split(features, n)
@@ -34,14 +34,14 @@ def cross_validation(n, features, labels, penalty):
 def score_stat(features, labels, test_size, penalty):
     """Train a SVM and get the score of classifying test_size data points
 
-        Args:
-            features: feature matrix
-            labels: labels for feature matrix
-            test_size: size of the testing set
-            penalty: penalty parameter for classifier
+    Args:
+        features: feature matrix
+        labels: labels for feature matrix
+        test_size: size of the testing set
+        penalty: penalty parameter for classifier
 
-        Returns:
-            accuracy of the model's classification of test_size data points
+    Returns:
+        accuracy of the model's classification of test_size data points
     """
     clf = svm.SVC(kernel='rbf', C=penalty).fit(features[:-test_size], labels[:-test_size])
     score = clf.score(features[-test_size:], labels[-test_size:])
@@ -52,16 +52,16 @@ def score_stat(features, labels, test_size, penalty):
 def permutation_test(num_sims, test_size, features, labels):
     """Run a permutation test   
 
-        Args:
-            num_sims: number of simulations to run
-            test_size: number of data points to test over
-            features: feature matrix
-            labels: labels for feature matrix
+    Args:
+        num_sims: number of simulations to run
+        test_size: number of data points to test over
+        features: feature matrix
+        labels: labels for feature matrix
 
-        Returns: 
-            p-value for the permutation test, i.e. how many tests had 
-            a test statistic greater than or equal to the original 
-            test statistic
+    Returns: 
+        p-value for the permutation test, i.e. how many tests had 
+        a test statistic greater than or equal to the original 
+        test statistic
     """
     la = labels.copy()
     test_stat = score_stat(features, la, test_size, 1.0)
